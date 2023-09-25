2d7233c00b0d9766b7ee220a91318a468c030b9b
PLWEP <langgeng86@gmail.com>
2023-09-25 11:35:12 +0700

    merubah tampilan profil

--- lib/src/view/profile_view.dart
+++ lib/src/view/profile_view.dart
@@ -5,8 +5,59 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profil"),
+    return Column(
+      children: [
+        Expanded(
+          child: Container(
+            color: Colors.blue,
+            padding: const EdgeInsets.only(
+              bottom: 15,
+              left: 10,
+            ),
+            child: const Align(
+              alignment: Alignment.bottomLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 100,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "Nama Pengguna",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        const Expanded(
+          flex: 2,
+          child: Column(
+            children: [
+              ListTile(
+                title: Text("Edit Profile"),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
