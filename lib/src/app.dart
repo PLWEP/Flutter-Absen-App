bea9527a0d6d748f6822b39b8483901d74c808c5
PLWEP <langgeng86@gmail.com>
2023-10-14 14:49:46 +0700

    delete unused file

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,54 +7,43 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'settings/settings_controller.dart';
-
 class MyApp extends StatelessWidget {
   const MyApp({
     super.key,
-    required this.settingsController,
   });
-
-  final SettingsController settingsController;
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: settingsController,
-      builder: (BuildContext context, Widget? child) {
-        return MaterialApp(
-          restorationScopeId: 'app',
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('en', ''),
-          ],
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
-          themeMode: settingsController.themeMode,
-          onGenerateRoute: (RouteSettings routeSettings) {
-            return MaterialPageRoute<void>(
-              settings: routeSettings,
-              builder: (BuildContext context) {
-                switch (routeSettings.name) {
-                  case ItemDetailsView.routeName:
-                    return ItemDetailsView(
-                      item: ModalRoute.of(context)?.settings.arguments
-                          as MataKuliah,
-                    );
-                  case ProfileView.routeName:
-                    return const ProfileView();
-                  default:
-                    return HomeView();
-                }
-              },
-            );
+    return MaterialApp(
+      restorationScopeId: 'app',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+      ],
+      onGenerateTitle: (BuildContext context) =>
+          AppLocalizations.of(context)!.appTitle,
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      onGenerateRoute: (RouteSettings routeSettings) {
+        return MaterialPageRoute<void>(
+          settings: routeSettings,
+          builder: (BuildContext context) {
+            switch (routeSettings.name) {
+              case ItemDetailsView.routeName:
+                return ItemDetailsView(
+                  item:
+                      ModalRoute.of(context)?.settings.arguments as MataKuliah,
+                );
+              case ProfileView.routeName:
+                return const ProfileView();
+              default:
+                return HomeView();
+            }
           },
         );
       },
